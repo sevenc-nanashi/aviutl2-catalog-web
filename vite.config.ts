f65b35cc00a109e1b5a527773f340a1769de687c
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-08-08 08:03:58 +0900

    docs: ライセンスを記述する

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+import path from "node:path";
 import vue from "@vitejs/plugin-vue";
 import vike from "vike/plugin";
 import uno from "unocss/vite";
+import license from "rollup-plugin-license";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 
@@ -12,5 +14,28 @@ export default defineConfig({
     vike(),
     vue(),
     uno(),
+    license({
+      cwd: process.cwd(),
+
+      thirdParty: {
+        output: {
+          file: path.join(
+            import.meta.dirname,
+            "dist",
+            "client",
+            "THIRD_PARTY_NOTICES.txt",
+          ),
+          encoding: "utf-8",
+        },
+      },
+    }),
   ],
+  build: {
+    rolldownOptions: {
+      output: {
+        postBanner:
+          "/** Check out the THIRD_PARTY_NOTICES.txt file for License information. */",
+      },
+    },
+  },
 });
